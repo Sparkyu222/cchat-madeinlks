cf7cee72d1c6858e792cb65ddbe11f02e25a2698
Lukas <sparkyu222@gmail.com>
2022-11-11 23:13:50 +0400

    added argument in main

--- src/cchat.c
+++ src/cchat.c
@@ -9,19 +9,50 @@
 #include "cserver.h"
 #include "cclient.h"
 
-int main () {
-  int choice;
-  signal(SIGINT, term);
-  puts("Cchat "RED"v4"RESET" madeinlks");
+int main (int argc, char **argv) {
+  int opt;
 
-  puts("Sélectionnez votre mode (1 = Serveur, 0 = Client):");
-  scanf("%d",&choice);
-  if (choice == 1) {
-    fserver();
+  if ( argc > 2 ) {
+    puts(RED"Syntaxe incorrecte"RESET);
+    puts("Utilisation :");
+    puts("-s\t -- Active le mode serveur");
+    puts("-c\t -- Active le mode client");
+    puts("-h\t -- Affiche cette page d'aide");
+    exit(EXIT_FAILURE);
   }
   else {
-    fclient();
+    while ((opt = getopt (argc, argv, ":sch")) != -1) {
+      switch (opt) {
+        case 's' :
+          puts("Cchat "RED"v4"RESET" madeinlks");
+          puts("Mode serveur activé");
+          fserver();
+          break;
+        case 'c' :
+          puts("Cchat "RED"v4"RESET" madeinlks");
+          puts("Mode client activé");
+          fclient();
+          break;
+        case 'h' :
+          puts("Utilisation :");
+          puts("-s\t -- Active le mode serveur");
+          puts("-c\t -- Active le mode client");
+          puts("-h\t -- Affiche cette page d'aide");
+          exit(EXIT_SUCCESS);
+          break;
+        default :
+          puts(RED"Syntaxe incorrecte"RESET);
+          puts("Utilisation :");
+          puts("-s\t -- Active le mode serveur");
+          puts("-c\t -- Active le mode client");
+          puts("-h\t -- Affiche cette page d'aide");
+          exit(EXIT_FAILURE);
+      }
+    }
   }
+
+  signal(SIGINT, term);
+  puts("Cchat "RED"v4"RESET" madeinlks");
 
   while (1) {                                                                                     // Boucle qui attent l'arrivée du message de fermeture de la part du client
     if (killthr == true) {
